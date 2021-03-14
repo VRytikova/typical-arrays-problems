556b3eb3b2db2916995ce54f1404404b484a6962
RVeronica <akinrv16@gmail.com>
2021-03-14 23:15:04 +0300

    feat: implement min, max and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
-
 exports.min = function min (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    } else{
+        let minNumber=array[0];
+        for(let i=1; i<array.length; i++){
+            if(minNumber > array[i]){
+                minNumber=array[i];
+            }
+        }
+        return minNumber;
+    }
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    } else{
+        let maxNumber=array[0];
+        for(let i=1; i<array.length; i++) {
+            if (maxNumber < array[i]) {
+                maxNumber = array[i];
+            }
+        }
+        return maxNumber;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array === undefined || array.length === 0){
+        return 0;
+    } else {
+        let avgNumber = 0;
+        for (let i = 0; i < array.length; i++) {
+            {
+                avgNumber = avgNumber + array[i];
+            }
+        }
+        return (avgNumber/(array.length));
+    }
 }
